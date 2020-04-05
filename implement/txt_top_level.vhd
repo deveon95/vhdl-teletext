390eb6cd3af223fcc3e644ccf84a5b5f4fb92d86
deveon95 <51299713+deveon95@users.noreply.github.com>
2020-04-05 15:23:55 +0100

    Created VGA video generator and simple colour bar test pattern

--- implement/txt_top_level.vhd
+++ implement/txt_top_level.vhd
@@ -1,4 +1,5 @@
 -- ********** Use Clock Controller to set the CLK1 (not CLK2) freq to 27750 on C10 Dev Board **
+-- ********** Use Clock Controller to set CLK2 to 28800
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -7,6 +8,7 @@ use ieee.numeric_std.all;
 entity TXT_TOP_LEVEL is
     port (
     CLK_27_750      : in  std_logic;
+    CLK_25          : in  std_logic;
     RESETn          : in  std_logic;
     CLK_REPEATER    : out std_logic;
     CLK_REPEATER2   : out std_logic;
@@ -14,25 +16,107 @@ entity TXT_TOP_LEVEL is
     RX_IN           : in  std_logic;
     SERIAL_DATA_OUT : out std_logic;
     SERIAL_CLOCK_OUT : out std_logic;
-    FRAME_VALID_OUT : out std_logic
+    FRAME_VALID_OUT : out std_logic;
+    
+    R_OUT : out std_logic;
+    G_OUT : out std_logic;
+    B_OUT : out std_logic;
+    HSYNC_OUT : out std_logic;
+    VSYNC_OUT : out std_logic
     );
 end entity TXT_TOP_LEVEL;
 
 architecture rtl of TXT_TOP_LEVEL is
+signal RESET : std_logic;
+signal R : std_logic;
+signal G : std_logic;
+signal B : std_logic;
+signal NEW_ROW : std_logic;
+signal PIXEL_COUNTER : integer range 0 to 767;
 begin
 
     CLK_REPEATER <= CLK_27_750;
     CLK_REPEATER2 <= CLK_27_750;
+    RESET <= not RESETn;
     RESET_REPEATER <= RESETn;
 
 DATA_RECOVERY: entity work.TXT_DATA_RECOVERY
     port map(
-    RESET => not RESETn,
+    RESET => RESET,
     CLK_27_750 => CLK_27_750,
     RX_IN => RX_IN,
     SERIAL_DATA_OUT => SERIAL_DATA_OUT,
     SERIAL_CLOCK_OUT => SERIAL_CLOCK_OUT,
     FRAME_VALID_OUT => FRAME_VALID_OUT);
+
+VGA: entity work.VGA
+    port map(
+    RESET => RESET,
+    CLK => CLK_25,
+    R_IN => R,
+    G_IN => G,
+    B_IN => B,
+    NEW_ROW_OUT => NEW_ROW,
+    NEW_SCREEN_OUT => open,
+    R_OUT => R_OUT,
+    G_OUT => G_OUT,
+    B_OUT => B_OUT,
+    HSYNC_OUT => HSYNC_OUT,
+    VSYNC_OUT => VSYNC_OUT);
+
+PATTERN_GEN: process(CLK_25, RESET)
+    begin
+        if RESET = '1' then
+            R <= '0';
+            G <= '0';
+            B <= '0';
+            PIXEL_COUNTER <= 0;
+        elsif rising_edge(CLK_25) then
+            if NEW_ROW = '1' then
+                PIXEL_COUNTER <= 0;
+            else
+                PIXEL_COUNTER <= PIXEL_COUNTER + 1;
+                if PIXEL_COUNTER < 96 then
+                    R <= '1';
+                    G <= '0';
+                    B <= '0';
+                elsif PIXEL_COUNTER < 192 then
+                    R <= '0';
+                    G <= '1';
+                    B <= '0';
+                elsif PIXEL_COUNTER < 288 then
+                    R <= '1';
+                    G <= '1';
+                    B <= '0';
+                elsif PIXEL_COUNTER < 384 then
+                    R <= '0';
+                    G <= '0';
+                    B <= '1';
+                elsif PIXEL_COUNTER < 480 then
+                    R <= '1';
+                    G <= '0';
+                    B <= '1';
+                elsif PIXEL_COUNTER < 576 then
+                    R <= '0';
+                    G <= '1';
+                    B <= '1';
+                elsif PIXEL_COUNTER < 672 then
+                    R <= '1';
+                    G <= '1';
+                    B <= '1';
+                elsif PIXEL_COUNTER < 767 then
+                    R <= '0';
+                    G <= '0';
+                    B <= '0';
+                else
+                    -- Generate vertical white line at right edge of screen
+                    R <= '1';
+                    G <= '1';
+                    B <= '1';
+                end if;
+            end if;
+        end if;
+    end process;
 
 end architecture;
     
---
+++ src/rtl/VGA.vhd
@@ -0,0 +1,122 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity VGA is
+    port (
+    CLK             : in  std_logic;
+    RESET           : in  std_logic;
+    R_IN            : in std_logic;
+    G_IN            : in std_logic;
+    B_IN            : in std_logic;
+    NEW_ROW_OUT     : out std_logic;
+    NEW_SCREEN_OUT  : out std_logic;
+    R_OUT           : out std_logic;
+    G_OUT           : out std_logic;
+    B_OUT           : out std_logic;
+    HSYNC_OUT       : out std_logic;
+    VSYNC_OUT       : out std_logic);
+end entity VGA;
+
+architecture RTL of VGA is
+constant H_SIZE : integer := 768;
+constant H_FRONT_PORCH : integer := 16;
+constant H_SYNC_PULSE : integer := 96;
+constant H_BACK_PORCH : integer := 48;
+constant V_SIZE : integer := 576;
+constant V_FRONT_PORCH : integer := 11;
+constant V_SYNC_PULSE : integer := 2;
+constant V_BACK_PORCH : integer := 31;
+--constant H_SIZE : integer := 640;
+--constant H_FRONT_PORCH : integer := 16;
+--constant H_SYNC_PULSE : integer := 96;
+--constant H_BACK_PORCH : integer := 48;
+--constant V_SIZE : integer := 480;
+--constant V_FRONT_PORCH : integer := 11;
+--constant V_SYNC_PULSE : integer := 2;
+--constant V_BACK_PORCH : integer := 31;
+
+constant H_COUNT_MAX : integer := H_SIZE + H_FRONT_PORCH + H_SYNC_PULSE + H_BACK_PORCH - 1;
+signal H_COUNT : integer range 0 to H_COUNT_MAX;
+signal H_ACTIVE : std_logic;
+constant V_COUNT_MAX : integer := V_SIZE + V_FRONT_PORCH + V_SYNC_PULSE + V_BACK_PORCH - 1;
+signal V_COUNT : integer range 0 to V_COUNT_MAX;
+signal V_ACTIVE : std_logic;
+signal VIDEO_ACTIVE : std_logic;
+
+begin
+VIDEO_ACTIVE <= H_ACTIVE and V_ACTIVE;
+R_OUT <= R_IN when VIDEO_ACTIVE = '1' else '0';
+G_OUT <= G_IN when VIDEO_ACTIVE = '1' else '0';
+B_OUT <= B_IN when VIDEO_ACTIVE = '1' else '0';
+
+COUNTER: process (CLK, RESET)
+    begin
+        if RESET = '1' then
+            NEW_ROW_OUT <= '0';
+            NEW_SCREEN_OUT <= '0';
+            H_COUNT <= 0;
+            V_COUNT <= 0;
+        elsif rising_edge(CLK) then
+            if H_COUNT = H_COUNT_MAX - 1 then
+                -- New Row Out goes high for one clock cycle
+                NEW_ROW_OUT <= '1';
+                H_COUNT <= H_COUNT + 1;
+            elsif H_COUNT = H_COUNT_MAX then
+                H_COUNT <= 0;
+                if V_COUNT = V_COUNT_MAX - 1 then
+                    -- New Screen Out goes high for one line
+                    NEW_SCREEN_OUT <= '1';
+                    V_COUNT <= V_COUNT + 1;
+                elsif V_COUNT = V_COUNT_MAX then
+                    V_COUNT <= 0;
+                    NEW_SCREEN_OUT <= '0';
+                else
+                    V_COUNT <= V_COUNT + 1;
+                end if;
+                NEW_ROW_OUT <= '0';
+            else
+                H_COUNT <= H_COUNT + 1;
+            end if;
+        end if;
+    end process;
+    
+SYNC_GENERATOR: process (CLK, RESET)
+    begin
+        if RESET = '1' then
+            H_ACTIVE <= '0';
+            HSYNC_OUT <= '1';
+            V_ACTIVE <= '0';
+            VSYNC_OUT <= '1';
+        elsif rising_edge(CLK) then
+            if H_COUNT < H_SIZE then
+                H_ACTIVE <= '1';
+                HSYNC_OUT <= '1';
+            elsif H_COUNT < H_SIZE + H_FRONT_PORCH then
+                H_ACTIVE <= '0';
+                HSYNC_OUT <= '1';
+            elsif H_COUNT < H_SIZE + H_FRONT_PORCH + H_SYNC_PULSE then
+                H_ACTIVE <= '0';
+                HSYNC_OUT <= '0';
+            else
+                H_ACTIVE <= '0';
+                HSYNC_OUT <= '1';
+            end if;
+            
+            if V_COUNT < V_SIZE then
+                V_ACTIVE <= '1';
+                VSYNC_OUT <= '1';
+            elsif V_COUNT < V_SIZE + V_FRONT_PORCH then
+                V_ACTIVE <= '0';
+                VSYNC_OUT <= '1';
+            elsif V_COUNT < V_SIZE + V_FRONT_PORCH + V_SYNC_PULSE then
+                V_ACTIVE <= '0';
+                VSYNC_OUT <= '0';
+            else
+                V_ACTIVE <= '0';
+                VSYNC_OUT <= '1';
+            end if;
+        end if;
+    end process;
+    
+end architecture;
